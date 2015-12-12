d50991378ec1fe18cebd2233e280e451a54b7b1c
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-12 22:00:06 +0100

    testing clanwars

--- runner.php
+++ runner.php
@@ -5,8 +5,9 @@ require_once "state/PlayerStats.php";
 
 //$game_counter = new GameCounter();
 $playerstats = new PlayerStatsAccumulator();
+$clanwars = new ClanwarsAccumulator();
 
 $reference = new ActionFPS\GamesCachedActionReference();
 $proc = new ActionFPS\Processor();
 echo json_encode($proc->processFromScratch($reference, $playerstats)->getState());
-
+echo json_encode($proc->processFromScratch($reference, $clanwars)->getState());

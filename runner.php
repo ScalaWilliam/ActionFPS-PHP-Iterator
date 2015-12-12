8119c1554c34ed8266e33698717278025c36923c
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-12 21:38:46 +0100

    integrating playerstats reducer

--- runner.php
+++ runner.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 header("Content-Type: application/json");
-require_once "GameCounter.php";
-$game_counter = new GameCounter();
+require_once "state/PlayerStats.php";
+
+//$game_counter = new GameCounter();
+$playerstats = new PlayerStatsAccumulator();
+
 $reference = new ActionFPS\GamesCachedActionReference();
 $proc = new ActionFPS\Processor();
-echo json_encode($proc->processFromScratch($reference, $game_counter)->getState());
+echo json_encode($proc->processFromScratch($reference, $playerstats)->getState());
 

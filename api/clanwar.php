68538413ff0c88edff2e1a447d6dd7a8a5e13fff
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-22 17:47:34 +0100

    clanwar API bug fix

--- api/clanwar.php
+++ api/clanwar.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require  __DIR__ . '/common.php';
 
 $selected = get_clanwars(null, null, null, $_GET['id']);
-echo json_encode(selected[$_GET['id']]);
+echo json_encode($selected[$_GET['id']]);

b676d2943d7dde0d7142e106dae20f92013698cd
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-22 18:23:20 +0100

    clanstats count

--- api/clanstats.php
+++ api/clanstats.php
@@ -3,6 +3,6 @@ header("Content-Type: application/json");
 require_once __DIR__ . '/../vendor/autoload.php';
 require  __DIR__ . '/common.php';
 
-$stats = get_clanstats(15);
+$stats = get_clanstats(!empty($_GET['count']) ? (int)$_GET['count'] : 15);
 
 echo json_encode($stats);

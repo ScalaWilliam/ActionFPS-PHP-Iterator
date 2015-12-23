19e8736c97e2f20bed675c9d8282f22d29afcca6
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-23 14:44:39 +0100

    clan: print last 15 clanwars

--- api/clan.php
+++ api/clan.php
@@ -8,7 +8,7 @@ $clan_id = $_GET['id'];
 
 $clan = find_clan($clan_id);
 
-$clan->wars = get_clanwars(10, null, $clan_id);
+$clan->wars = get_clanwars(15, null, $clan_id);
 $clan->stats = get_clanstats(0, $clan_id)->now;
 
 echo json_encode($clan);
